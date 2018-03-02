4c853b0a0f14c38f3084f8c60db5f53592a68bc0
Roger <rogerren1842@hotmail.com>
2018-03-03 01:06:00 +0800

    建立控制器(展示功能)

---
+++ Demo.Solution/Demo.Web/Controllers/DemoController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Demo.Web.Controllers
+{
+    /// <summary>
+    /// 控制器(展示功能)
+    /// </summary>
+    public class DemoController : Controller
+    {
+        /// <summary>
+        /// 首頁
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
